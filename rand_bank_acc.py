72bfee703687531dde4198281de82c80f4597209
Tomasz Tracz <tomasz.tracz@2itesting.com>
2023-03-17 11:56:01 +0000

    Clean up the code

--- rand_bank_acc.py
+++ rand_bank_acc.py
@@ -3,37 +3,42 @@ from random_uk_bank_account import GenerateUkBankAccount
 from sort_code_from_csv import random_sort_code
 from params import get_params
 
-def random_bank_accounts(code, total):
-    """Return random bank account number."""
-    sort_codes = {}
-    for _ in range(0, code):
-        while True:
-            sort_code, bank_acc = random_bank_acc(total)
-            if sort_code not in sort_codes:
-                break
-        
-        sort_codes[sort_code] = bank_acc
 
-    return sort_codes
+class RandomSortCodes:
+    def __init__(self, sort_code_qty, bank_account_qty):
+        self.sort_code_qty = sort_code_qty
+        self.bank_account_qty = bank_account_qty
+        self.sort_codes = {}
 
-def random_bank_acc(total):
-    """Return random bank account number."""
-    sort_code = random_sort_code()
-    try:
-        return (sort_code, GenerateUkBankAccount().generate_for_sort_code(sort_code, total=total).account_numbers)
-    except:
-        return random_bank_acc(total)
-    
-def save_to_csv(data, file):
-    """Save data to csv file."""
-    with open(file, "w") as f:
-        writer = csv.writer(f)
-        writer.writerow(["Sort Code", "Account Number"])
-        for sort_code, bank_acc in data.items():
-            for acc in bank_acc:
-                writer.writerow([sort_code, acc])
+    def generate(self):
+        sort_code_list = {}
+        for _ in range(0, self.sort_code_qty):
+            while True:
+                sort_code, bank_acc = self.__random_sort_code_with_bank_accounts()
+                if sort_code not in sort_code_list:
+                    break
+            sort_code_list[sort_code] = bank_acc
+        self.sort_codes = sort_code_list
+        return sort_code_list
+
+    def __random_sort_code_with_bank_accounts(self):
+        sort_code = random_sort_code()
+        try:
+            return (sort_code, GenerateUkBankAccount().generate_for_sort_code(sort_code, total=self.bank_account_qty).account_numbers)
+        except:
+            return self.__random_sort_code_with_bank_accounts()
+
+    def save_to_csv(self, file):
+        with open(file, "w") as f:
+            writer = csv.writer(f)
+            writer.writerow(["Sort Code", "Account Number"])
+            for sort_code, bank_acc in self.sort_codes.items():
+                for acc in bank_acc:
+                    writer.writerow([sort_code, acc])
 
 if __name__ == "__main__":
     codes, accounts, file = get_params()
-    generated_accounts = random_bank_accounts(codes, accounts)
-    save_to_csv(generated_accounts, file)
+
+    random_sort_codes = RandomSortCodes(codes, accounts)
+    random_sort_codes.generate()
+    random_sort_codes.save_to_csv(file)
